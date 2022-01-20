9c75bbadde9c32bf7c7118c4dcbf180ae259bf80
hafidznrg <hafidznrg.edu@gmail.com>
2022-01-20 18:18:24 +0700

    completing search algo

--- main.cpp
+++ main.cpp
@@ -30,12 +30,67 @@ void processWord(string line, vector<string> &word){
     word.push_back(line);
 }
 
+bool check(vector<vector<char>> mat, string word, int i, int j, int ver, int hor){
+    int idx = 1;
+    int n = word.length();
+    while (idx < n && word[idx] == mat[i+ver*idx][j+hor*idx]){
+        idx++;
+    }
+    if (idx == n){
+        cout << word << " (" << i << "," << j << ")" << endl;
+        return true;
+    } else {
+        return false;
+    }
+}
 
-void checkWord(vector<vector<char>> mat, string word, int row, int col, int* totalComparison){
+
+void checkWord(vector<vector<char>> mat, string word, int row, int col){
+    int n = word.length();
+    bool found = false;
     for (int i=0; i<mat.size(); i++){
         for (int j=0; j<mat[0].size(); j++){
             if (mat[i][j] == word[0]){
                 // checking all possibilities
+                // horizontal kanan
+                if (col-j >= n && !found){
+                    found = check(mat, word, i, j, 0, 1);
+                }
+
+                // horizontal kiri
+                if (j+1 >= n && !found){
+                    found = check(mat, word, i, j, 0, -1);
+                }
+
+                // vertikal atas
+                if (i+1 >= n && !found){
+                    found = check(mat, word, i, j, -1, 0);
+                }
+
+                // vertikal bawah
+                if (row-i >= n && !found){
+                    found = check(mat, word, i, j, 1, 0);
+                }
+
+                // miring kanan atas
+                if (col-j >= n && i+1 >= n && !found){
+                    found = check(mat, word, i, j, -1, 1);
+                }
+
+                // miring kanan bawah
+                if (col-j >= n && row-i >= n && !found){
+                    found = check(mat, word, i, j, 1, 1);
+                }
+
+                // miring kiri atas
+                if (j+1 >= n && i+1 >= n && !found){
+                    found = check(mat, word, i, j, -1, -1);
+                }
+
+                // miring kiri bawah
+                if (j+1 >= n && row-i >= n && !found){
+                    found = check(mat, word, i, j, 1, -1);
+                }
             }
         }
     }
@@ -75,12 +130,13 @@ int main(){
     int totalComparison = 0;
 
     for (int i=0; i< word.size(); i++){
-        checkWord(mat, word[i], row, col, &totalComparison);
+        checkWord(mat, word[i], row, col);
     }
 
     // stop timer
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
 
-    cout << "Total durasi pencarian:" << duration.count() << " microseconds" << endl;
+    cout << "Total perbandingan : " << totalComparison << endl;
+    cout << "Total durasi pencarian : " << duration.count() << " microseconds" << endl;
 }
